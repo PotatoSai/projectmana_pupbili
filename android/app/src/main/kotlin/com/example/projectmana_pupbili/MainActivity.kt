b504943ca1c77831581dc1dd065ab3f94a853a2b
PotatoSai <kirashiri2004@gmail.com>
2025-01-20 00:05:04 +0800

    logo, email and password textfields

---
+++ android/app/src/main/kotlin/com/example/projectmana_pupbili/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.projectmana_pupbili
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:projectmana_pupbili/components/my_text_field.dart';
+
+class LoginPage extends StatelessWidget {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //Logo
+            Icon(
+              Icons.lock_open_rounded,
+              size: 100,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+
+            const SizedBox(height: 25),
+
+            //Title or Slogan
+            Text("PUPBILI",
+              style: TextStyle(
+                fontSize: 16,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+            ),
+
+            const SizedBox(height: 25),
+
+            //Email
+            MyTextField(
+                controller: emailController,
+                hintText: "Email",
+                obscureText: false,
+            ),
+
+            const SizedBox(height: 10),
+
+            //Password
+            MyTextField(
+                controller: passwordController,
+                hintText: "Password",
+                obscureText: true),
+
+
+            //Sign In Button
+
+            const SizedBox(height: 10),
+
+            //Not Registered? Register Now
+          ],
+        ),
+      ),
+    );
+  }
+}
