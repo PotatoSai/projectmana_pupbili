b89b9f4b0cf4885e1cba38dcc342642ef2b451a1
PotatoSai <kirashiri2004@gmail.com>
2025-01-24 00:09:14 +0800

    updates

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:projectmana_pupbili/components/app_buttons.dart';
 import 'package:projectmana_pupbili/components/app_cart_tile.dart';
 import 'package:projectmana_pupbili/model/restaurant.dart';
 import 'package:provider/provider.dart';
@@ -15,27 +16,72 @@ class CartPage extends StatelessWidget {
       // scaffold
       return Scaffold(
         appBar: AppBar(
-          title: Text("Cart"),
+          title: const Text("Cart"),
           backgroundColor: Colors.transparent,
           foregroundColor: Theme.of(context).colorScheme.inversePrimary,
+          actions: [
+            //clear cart button
+            IconButton(
+                onPressed: () {
+                  showDialog(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                        title: const Text("Clear the cart?"),
+                        actions: [
+                          //cancel button
+                          TextButton(
+                              onPressed: () => Navigator.pop(context),
+                              child: const Text("Cancel"),),
+
+
+                          // yes button
+                          TextButton(
+                            onPressed: (){ Navigator.pop(context);
+                              restaurant.clearCart();
+                            },
+                            child: const Text("Yes"),),
+                        ],
+                      ),
+                  );
+                },
+                icon: const Icon(Icons.delete),
+            ),
+          ],
         ),
         body: Column(
           children: [
+
+            //list of cart
             Expanded(
-                child: ListView.builder(
-                  itemCount: userCart.length,
-                  itemBuilder: (context, index) {
-
-                    //get individual cart item
-                    final cartItem = userCart[index];
-
-                    //return cart tile UI
-                    return MyCartTile(cartItem: cartItem);
-                    }
-                  ),
+              child: Column(
+                children: [
+                  userCart.isEmpty
+                  ? const Expanded(
+                      child: Center(child: Text("Cart is empty.."),),)
+                      : Expanded(
+                      child: ListView.builder(
+                        itemCount: userCart.length,
+                        itemBuilder: (context, index) {
+              
+                          //get individual cart item
+                          final cartItem = userCart[index];
+              
+                          //return cart tile UI
+                          return MyCartTile(cartItem: cartItem);
+                          }
+                        ),
+                      ),
+                    ],
                 ),
-              ],
-          ),
+            ),
+
+            //button to pay
+            MyButton(
+                text: "Go to checkout",
+                onTap: (){}),
+            const SizedBox(height: 25,),
+          ],
+        ),
         );
     },);
   }
